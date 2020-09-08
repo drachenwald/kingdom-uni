34ecb26dcfecb2eeb4c65d6efdd66051cf6dd377
Anna Wilson <git@annawilson.eu>
2020-09-08 23:05:48 +0100

    mess with the footer

--- src/js/Layout.js
+++ src/js/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 
 import heraldry from '../images/uni-chancellor-badge.svg';
 import drachenwald_arms from '../images/drachenwald-arms.svg';
@@ -7,54 +7,59 @@ import drachenwald_arms from '../images/drachenwald-arms.svg';
 const Layout = (props) => {
   return (
     <>
-    <Navbar expand="lg" bg="drachenwald" variant="dark">
-      <Container>
-        <Navbar.Brand href="#/">
-          <img
-            alt=""
-            src={heraldry}
-            width="100"
-            className="d-inline-block align-middle"
-          />{' '}
-          Kingdom University
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Nav className="ml-auto">
-            <Nav.Link href="#/register" className="mx-2">Register to attend</Nav.Link>
-            <Nav.Link href="#/submitclass" className="mx-2">Submit a class</Nav.Link>
-            <Nav.Link href="#/schedule" className="mx-2">See the schedule</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar expand="lg" bg="drachenwald" variant="dark">
+        <Container>
+          <Navbar.Brand href="#/">
+            <img
+              alt=""
+              src={heraldry}
+              width="100"
+              className="d-inline-block align-middle"
+            />{' '}
+            Kingdom University
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className="ml-auto">
+              <Nav.Link href="#/register" className="mx-2">Register to attend</Nav.Link>
+              <Nav.Link href="#/submitclass" className="mx-2">Submit a class</Nav.Link>
+              <Nav.Link href="#/schedule" className="mx-2">See the schedule</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-    { props.children }
+      { props.children }
 
-    <br />
+      <br />
 
-    <Navbar expand="lg" bg="drachenwald" variant="dark">
-      <Container>
-        <Navbar.Brand href="https://drachenwald.sca.org/">
-          <img
-            alt=""
-            src={drachenwald_arms}
-            width="90"
-            className="d-inline-block align-middle"
-          />
-        </Navbar.Brand>
-        <Navbar.Text>
-          <br />
-          <p>
-            &copy; 2020 Drachenwald. This is a subsite of the recognised web site for the Kingdom of Drachenwald in the Society for Creative Anachronism.<br />
-            Copyright on all content and images remains with the creators.
-          </p>
-          <p>
-            <a href="https://drachenwald.sca.org/">Visit the main Drachenwald website.</a>
-          </p>
-        </Navbar.Text>
-      </Container>
-    </Navbar>
+      <Navbar expand="lg" bg="drachenwald" variant="dark">
+        <Container>
+          <Row>
+            <Col xs="auto">
+              <Navbar.Brand href="https://drachenwald.sca.org/">
+                <img
+                  alt=""
+                  src={drachenwald_arms}
+                  width="90"
+                  className="d-inline-block align-middle"
+                />
+              </Navbar.Brand>
+            </Col>
+            <Col className="align-middle">
+              <Navbar.Text className="align-middle">
+                <p>
+                  &copy; 2020 Drachenwald. This is a subsite of the recognised web site for the Kingdom of Drachenwald in the Society for Creative Anachronism.<br />
+                  Copyright on all content and images remains with the creators.
+                </p>
+                <p>
+                  <a href="https://drachenwald.sca.org/">Visit the main Drachenwald website.</a>
+                </p>
+              </Navbar.Text>
+            </Col>
+          </Row>
+        </Container>
+      </Navbar>
     </>
   );
 }
