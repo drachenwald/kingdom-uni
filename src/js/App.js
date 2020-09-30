8961b38f303e6e968e5e24430dbba8a238334cd8
Anna Wilson <git@annawilson.eu>
2020-09-30 22:41:46 +0100

    hashrouter to debug white screen of death

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { readRemoteFile } from 'react-papaparse';
 
 import Layout from './Layout';
