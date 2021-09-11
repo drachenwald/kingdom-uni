7b40bb23ac892e9f6f6124e31773ff2c00093880
MaryWK <marywk@gmail.com>
2021-09-11 11:59:38 +0000

    Update Housekeeping.js

--- src/js/Housekeeping.js
+++ src/js/Housekeeping.js
@@ -53,8 +53,16 @@ const Housekeeping = (props) => {
 </ul>
 <h2>Code of conduct</h2>
 <p>We observe the SCA Code of Conduct and expect all participants to:</p>
-<ul><li>act in accordance with the chivalric virtues of honor and service;</li><li>value and respect the worth and dignity of all individuals;</li><li>practice inclusiveness and respect pluralism and diversity;</li><li>promote a safe and respectful environment for all SCA events;</li><li>act with transparency, fairness, integrity and honesty;</li><li>be a responsible steward of SCA resources; and,</li><li>be committed to maintaining the trust of its members and participants.</li></ul>
-	<p>Breaches and disruptive behaviour will see offenders removed and the incident reported. We really hope this won’t be necessary.</p><p>We hope Kingdom University  offers the highest hospitality this fair Kingdom can provide online. So, please, maintain honour, respect and decorum and relax into a long weekend of learning and enlightenment!</p>
+<ul><li>act in accordance with the chivalric virtues of honor and service;</li>
+<li>value and respect the worth and dignity of all individuals;</li>
+<li>practice inclusiveness and respect pluralism and diversity;</li>
+<li>promote a safe and respectful environment for all SCA events;</li>
+<li>act with transparency, fairness, integrity and honesty;</li>
+<li>be a responsible steward of SCA resources; and,</li>
+<li>be committed to maintaining the trust of its members and participants.</li>
+</ul>
+	<p>Breaches and disruptive behaviour will see offenders removed and the incident reported. We really hope this won’t be necessary.</p>
+  <p>We hope Kingdom University  offers the highest hospitality this fair Kingdom can provide online. So, please, maintain honour, respect and decorum and relax into a long weekend of learning and enlightenment!</p>
 <p>All the best from your organising team!</p>
 
             <p>
