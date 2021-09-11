4dce5928076263bd76f06db31029314db2ef1efc
MaryWK <marywk@gmail.com>
2021-09-11 11:55:17 +0000

    Update Housekeeping.js with new intro, formatting, and link to call for classes.

--- src/js/Housekeeping.js
+++ src/js/Housekeeping.js
@@ -19,37 +19,46 @@ const Housekeeping = (props) => {
           </Col>
 
           <Col>
-            <p>Dear all,<br />
-              we are all very excited to be bringing together nearly 500 attendees across three days of programming on eight core Zoom channels, as well as the Discord channels.</p>
-            <p>There are about 70 or so teachers, as well as volunteers to help with technical support. Please note that everyone is a volunteer, and our technical team will be doing their best to resolve technical issues.</p>
-            <p>We also want this to be a friendly, fun and respectful event, and ask that you help us deliver the smoothest possible content with the following Housekeeping steps:</p>
-            <p>Keep passwords and Zoom links private – if someone approaches you saying they have lost their email or password, please ask them to contact us directly.</p>
-            <p>Should a Zoom channel be disrupted, please leave the session and wait for alternate links – whilst we will endeavour to replace links, it might not always be possible to restart a session. Please bear with us if this happens while we restore that stream.</p>
-            <p>Period clothing is recommended, but is not compulsory, particularly if you are new, or needing to go to/from mundane activities due to time zone differences.</p>
-            <p>Please be prompt in arriving at the start of a session to pick up on teacher preferences – e.g. how to ask questions.</p>
-            <p>If a session is over-running, please be patient. The support will be encouraging a prompt end to the session.</p>
-            <p>Some sessions will be recorded – Our recommendation if you do not wish to be seen on this, is to switch off your video feed and mic. </p>
-            <p>Should there be bandwidth issues, please turn off your video, it helps reduce congestion and lag.</p>
-            <p>Please set your mic to mute until asked to un-mute for questions – unintentional and background sounds will cause the view window to jump to your camera.</p>
-            <p>Some teachers will prefer that you ask questions in the chat, this is a good way to jot down that question before you forget it in time for the Q&amp;A.</p>
-            <p>If you have additional questions, or were not called during the session, there will be a Discord channel for additional questions and it is a good place to add them there. </p>
-            <p>We observe the SCA Code of Conduct and expect participants to:</p>
+            
 
-            <ul>
-              <li>act in accordance with the chivalric virtues of honor and service;</li>
-              <li>value and respect the worth and dignity of all individuals;</li>
-              <li>practice inclusiveness and respect pluralism and diversity;</li>
-              <li>promote a safe and respectful environment for all SCA events;</li>
-              <li>act with transparency, fairness, integrity and honesty;</li>
-              <li>be a responsible steward of SCA resources; and,</li>
-              <li>be committed to maintaining the trust of its members and participants.</li>
-            </ul>
+<br />
+            <Alert variant="secondary">
+              
+              <p>Dear all, <br />
+                we would like to invite everyone to attend another exciting digital Drachenwald Kingdom University event, 19-21 November 2021. The classes will be a combination of live and recorded classes held on Zoom channels and using a Discord server for chat and socialising during the event. </p>
+              <p>Please consider offering a class for this event. Classes can be either designed as live, or as pre-recorded with a Q&A session live during the event. 
 
-            <p>Breaches and disruptive behaviour will see offenders removed and the incident reported. We really hope this won’t be necessary, and that it offers the highest hospitality this fair Kingdom can provide online. So, please, maintain honour, respect and decorum and relax into a long weekend of learning and enlightenment!</p>
-            <p>All the best from your organising team!</p>
+              </p>
+              <p><LinkContainer to="/callforclasses"><Button variant="primary"><b>View call for classes, including topics</b></Button></LinkContainer>
+              </p>
+
+            </Alert>
+
+
+
+            <h2>Housekeeping</h2>
+  <p>We are all very excited to be bringing you another event via Zoom and Discord channels. We  want this to be a friendly, fun and respectful event, and ask that you help us deliver the smoothest possible content with the following Housekeeping steps:</p>
+<ul>
+  <li>Please remember that everyone is a volunteer, and our technical team will be doing their best to resolve technical issues.</li>
+  <li>Keep passwords and Zoom links private – if someone approaches you saying they have lost their email or password, please ask them to contact us directly.</li>
+<li>If a Zoom channel is disrupted, please leave the session and wait for alternate links – whilst we will endeavour to replace links, it might not always be possible to restart a session. Please bear with us if this happens while we restore that stream. Messages will be posted on Discord, and where possible, facebook.</li>
+  <li>Period clothing is recommended, but is not compulsory, particularly if you are new, or needing to go to/from mundane activities due to time zone differences.</li>
+  <li>Please be prompt in arriving at the start of a session to pick up on teacher preferences, such as how to ask questions. Also, please remember to leave promptly at the end of each session. </li>
+  <li>If a session is over-running, please be patient. The support team will be encouraging a prompt end to the session.</li>
+  <li>Some sessions may be recorded. If you do not wish to be seen on the recording, we recommend that you switch off your video feed and mic. </li>
+  <li>If there are bandwidth issues, please turn off your video; it helps reduce congestion and lag.</li>
+  <li>Please set your mic to mute until asked to un-mute for questions. Unintentional and background sounds will cause the view window to jump to your camera.</li>
+  <li>Some teachers will prefer that you ask questions in the chat. This is a good way to jot down that question before you forget it in time for the Q&amp;A.</li>
+  <li>If you have additional questions, or were not called during the session, there will be a Discord channel for  extra questions. Please add your additional questions to that channel, and keep the discussion going! </li>
+</ul>
+<h2>Code of conduct</h2>
+<p>We observe the SCA Code of Conduct and expect all participants to:</p>
+<ul><li>act in accordance with the chivalric virtues of honor and service;</li><li>value and respect the worth and dignity of all individuals;</li><li>practice inclusiveness and respect pluralism and diversity;</li><li>promote a safe and respectful environment for all SCA events;</li><li>act with transparency, fairness, integrity and honesty;</li><li>be a responsible steward of SCA resources; and,</li><li>be committed to maintaining the trust of its members and participants.</li></ul>
+	<p>Breaches and disruptive behaviour will see offenders removed and the incident reported. We really hope this won’t be necessary.</p><p>We hope Kingdom University  offers the highest hospitality this fair Kingdom can provide online. So, please, maintain honour, respect and decorum and relax into a long weekend of learning and enlightenment!</p>
+<p>All the best from your organising team!</p>
 
             <p>
-              <LinkContainer to="/schedule"><Button variant="primary"><b>See the schedule</b></Button></LinkContainer>
+              <LinkContainer to="/callforclasses"><Button variant="primary"><b>View call for classes</b></Button></LinkContainer>
             </p>
           </Col>
 
