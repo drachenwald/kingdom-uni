1addd7e1b40b02b1ff64616be41814dc8ab1062d
Anna Wilson <git@annawilson.eu>
2021-09-11 18:53:32 +0100

    need alert component

--- src/js/Housekeeping.js
+++ src/js/Housekeeping.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Alert } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import Banner from './Banner';
