7eb0b0b3d99a310fc5655a5ef4b5bebc36d46e8b
Mary Woodcock Kroble <mlw2@st-andrews.ac.uk>
2021-11-21 17:26:23 +0000

    Update Home.js

--- src/js/Home.js
+++ src/js/Home.js
@@ -36,42 +36,56 @@ const Home = (props) => {
           )
       }
 
-        <CardDeck>
-          <Card>
-            <Card.Img variant="top" src={goldwork} />
-            <Card.Body>
-              <Card.Title>Schedule</Card.Title>
-              <Card.Text>
-                View all classes running over the weekend.
-              </Card.Text>
-              <LinkContainer to="/schedule"><Button variant="primary"><b>Read more...</b></Button></LinkContainer>
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src={register} />
-            <Card.Body>
-              <Card.Title>Registration</Card.Title>
-              <Card.Text>
-                Sign up to attend. Submit a class if you want to teach.
-              </Card.Text>
-              <Button variant="primary" href="https://docs.google.com/forms/d/e/1FAIpQLSeZj4XFu_C8-zc5ejLYZL-kUQaGz4UqOyrR-_bBU5WF2XcA0g/viewform"><b>Register here</b></Button>
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src={food} />
-            <Card.Body>
-            <Card.Title>Housekeeping</Card.Title>
-              <Card.Text>
-               Guidelines for online conduct of teachers, assistants and attendees
-              </Card.Text>
-              <LinkContainer to="/housekeeping"><Button variant="primary"><b>Read about conduct</b></Button></LinkContainer>
-            </Card.Body>
-          </Card>
-        </CardDeck>
+      {// <CardDeck>
+      //    <Card>
+      //      <Card.Img variant="top" src={goldwork} />
+      //      <Card.Body>
+      //        <Card.Title>Schedule</Card.Title>
+      //        <Card.Text>
+      //          View all classes running over the weekend.
+      //        </Card.Text>
+      //        <LinkContainer to="/schedule"><Button variant="primary"><b>Read more...</b></Button></LinkContainer>
+      //      </Card.Body>
+      //    </Card>
+      //    <Card>
+      //      <Card.Img variant="top" src={register} />
+      //      <Card.Body>
+      //        <Card.Title>Registration</Card.Title>
+      //        <Card.Text>
+      //          Sign up to attend. Submit a class if you want to teach.
+      //        </Card.Text>
+      //        <Button variant="primary" href="https://docs.google.com/forms/d/e/1FAIpQLSeZj4XFu_C8-zc5ejLYZL-kUQaGz4UqOyrR-_bBU5WF2XcA0g/viewform"><b>Register here</b></Button>
+      //      </Card.Body>
+      //    </Card>
+      //    <Card>
+      //      <Card.Img variant="top" src={food} />
+      //      <Card.Body>
+      //      <Card.Title>Housekeeping</Card.Title>
+      //        <Card.Text>
+      //         Guidelines for online conduct of teachers, assistants and attendees
+      //        </Card.Text>
+      //        <LinkContainer to="/housekeeping"><Button variant="primary"><b>Read about conduct</b></Button></LinkContainer>
+      //      </Card.Body>
+      //    </Card>
+      //  </CardDeck>
+    }
 
         <br />
         <br />
+        <Row>
+          <Col xs={0} sm={1} xl={2}>
 
+          </Col>
+
+          <Col>
+             <h3>Kingdom University is now closed</h3>
+             <p>Thankyou to everyone - the teachers, the attendees and the event team for making this event such a special one. </p>
+          </Col>
+
+          <Col xs={0} sm={1} xl={2}>
+
+          </Col>
+        </Row>
 
 
         <Row>
