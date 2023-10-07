eb4b69879105ac97371750570c2529558ccf4525
dw-webminister-deputy <125492283+dw-webminister-deputy@users.noreply.github.com>
2023-10-07 14:32:03 +0100

    Update Callforclasses.js

--- src/js/Callforclasses.js
+++ src/js/Callforclasses.js
@@ -24,26 +24,29 @@ const Callforclasses = (props) => {
   <Alert variant="secondary" >
 
               <p>Dear all, <br />    
-                we would like to invite everyone to attend another exciting digital Drachenwald Kingdom University event, 19-21 November 2021. The classes will be a combination of live and recorded classes held on Zoom channels and using a Discord server for chat and socialising during the event. </p>
-              <p>Please consider offering a class for this event. Classes can be either designed as live, or as pre-recorded with a Q&A session live during the event. We'd love to see classes in:
+                We would like to invite everyone to attend another exciting digital Drachenwald Kingdom University event, 26-28 January 2024. The classes will be live classes held on Zoom channels.  
+                We will be using a Discord server for chat and socialising during the event. </p>
+              <p>Please consider offering a class for this event. We'd love to see classes in:
               </p>
               <ul>
-    <li>Sewing and Costuming</li>
+  <li>SCA and History</li>
+  <li>Sewing and Costuming</li>
   <li>Other Fibre Arts</li>
   <li>Science</li>
   <li>Fighting, Fencing and Archery</li>
-    <li>Music and Dance</li>
+ <li>Music and Dance</li>
   <li>Calligraphy, Illumination and Books</li>
-  <li>SCA and History</li>
   <li>Penguins (miscellaneous items) </li>
 </ul>
-              <p><Button variant="primary" href="https://docs.google.com/forms/d/e/1FAIpQLSeZj4XFu_C8-zc5ejLYZL-kUQaGz4UqOyrR-_bBU5WF2XcA0g/viewform"><b>Register and submit a class</b></Button>
+              <p><Button variant="primary" href="https://docs.google.com/forms/d/e/1FAIpQLSd6H5DKheYp-umZ7iffoEokFFXeW4cHU8ZGQExqwv9jFQ357g/viewform?fbclid=IwAR3E2IxvqiKLjFZJBG-6lsSVCM7Gawj1qy35DfTfdWaPTZ2iNCoBDRxvFYc">
+          <b>Submit a class</b></Button>
               </p>
 </Alert>
 
 
             <h2>Information on designing classes</h2>
-  <p> We would like to encourage both live and pre-recorded classes. The latter can have a live Q&A session scheduled during the event. If you are interested in creating a pre-recorded class, we have a number of people who are willing to give you guidance on the creation and editing of a short video. Please contact our fabulous videographer Artemis at <a href="mailto:Artemis.Swann@blinkingowlmedia.co.uk">Artemis.Swann@blinkingowlmedia.co.uk</a> for help and details. </p>
+  <p> We would like to encourage both live and pre-recorded classes. 
+The latter can have a live Q&A session scheduled during the event. If you are interested in creating a pre-recorded class, we have a number of people who are willing to give you guidance on the creation and editing of a short video. Please contact our fabulous videographer Artemis at <a href="mailto:Artemis.Swann@blinkingowlmedia.co.uk">Artemis.Swann@blinkingowlmedia.co.uk</a> for help and details. </p>
   <p>Please be aware that any recorded items, including both recordings of live sessions and pre-recorded material, must conform to copyright law. This means that all sources of images must be documented and permissions obtained where necessary. For a live presentation that is not being recorded, use for education as well as fair use applies; but if we record and post your video then we need to be careful that fair use is accurately applied. For guidance on how this works, here are some guidelines for the UK - note international rules may also apply:</p>
 <ul>
 <li><a href="https://copyrightservice.co.uk/copyright/p29_video_copyright">Video copyright</a></li>
@@ -56,7 +59,7 @@ const Callforclasses = (props) => {
 
 <p>All the best from your organising team!</p>
 
-            <p><Button variant="primary" href="https://docs.google.com/forms/d/e/1FAIpQLSeZj4XFu_C8-zc5ejLYZL-kUQaGz4UqOyrR-_bBU5WF2XcA0g/viewform"><b>Register and submit a class</b></Button>
+            <p><Button variant="primary" href="https://docs.google.com/forms/d/e/1FAIpQLSd6H5DKheYp-umZ7iffoEokFFXeW4cHU8ZGQExqwv9jFQ357g/viewform?fbclid=IwAR3E2IxvqiKLjFZJBG-6lsSVCM7Gawj1qy35DfTfdWaPTZ2iNCoBDRxvFYc"><b>Submit a class</b></Button>
               </p>
           </Col>
 
