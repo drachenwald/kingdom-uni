a73130857b7714811ee3cb2da399188ac3ef7514
dw-webminister-deputy <125492283+dw-webminister-deputy@users.noreply.github.com>
2024-01-19 12:50:08 +0000

    Update WhatsOnNow.js
    adjusting layout

--- src/js/WhatsOnNow.js
+++ src/js/WhatsOnNow.js
@@ -23,31 +23,31 @@ const WhatsOnNow = (props) => {
         </Col>
       </Row>
       <Row>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={4}>
           <WhatsOnNowRoom room="Mos Eisley Cantina" schedByRoom={props.schedByRoom} now={now} mutebutton={props.mutebutton} />
         </Col>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={4}>
           <WhatsOnNowRoom room="Alderaan" schedByRoom={props.schedByRoom} now={now} mutebutton={props.mutebutton} />
         </Col>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={4}>
           <WhatsOnNowRoom room="Coruscant" schedByRoom={props.schedByRoom} now={now} mutebutton={props.mutebutton} />
         </Col>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={4}>
           <WhatsOnNowRoom room="Dantooine" schedByRoom={props.schedByRoom} now={now} mutebutton={props.mutebutton} />
         </Col>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={4}>
           <WhatsOnNowRoom room="Endor" schedByRoom={props.schedByRoom} now={now} mutebutton={props.mutebutton} />
         </Col>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={4}>
           <WhatsOnNowRoom room="Geonosis" schedByRoom={props.schedByRoom} now={now} mutebutton={props.mutebutton} />
         </Col>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={4}>
           <WhatsOnNowRoom room="Hoth" schedByRoom={props.schedByRoom} now={now} mutebutton={props.mutebutton} />
         </Col>
-<Col xs={12} md={3}>
+        <Col xs={12} md={4}>
           <WhatsOnNowRoom room="Kamino" schedByRoom={props.schedByRoom} now={now} mutebutton={props.mutebutton} />
         </Col>
-<Col xs={12} md={3}>
+        <Col xs={12} md={4}>
           <WhatsOnNowRoom room="Tatooine" schedByRoom={props.schedByRoom} now={now} mutebutton={props.mutebutton} />
         </Col>
       </Row>
