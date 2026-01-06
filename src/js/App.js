eb3d1092e68c7890004f25cff0b6b3066d23b2a3
anna-wilson <30553780+anna-wilson@users.noreply.github.com>
2026-01-06 20:04:25 +0000

    Additional rooms

--- src/js/App.js
+++ src/js/App.js
@@ -22,7 +22,7 @@ function App() {
     'nickname': 'Event time',
   }
 
-  const roomnames = [ 'The Mended Drum', 'Ankh Morpork', 'Lancre', 'Klatch', 'Sto Lat', 'Genua', 'Quirm' ]
+  const roomnames = [ 'The Mended Drum', 'Ankh Morpork', 'Lancre', 'Klatch', 'Sto Lat', 'Genua', 'Quirm', 'The unseen university', 'The Chalk', 'Sto Helit' ]
 
   const scheduleUrl = 'https://dis.drachenwald.sca.org/data/ku.json';
 
